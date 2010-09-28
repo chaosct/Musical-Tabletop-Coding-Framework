057b6ac40e7fa8e807aeaff727eee4572d3f5409
Carles F. Julià <carles.fernandez@upf.edu>
2010-09-28 12:23:38 +0200

    License on c++ sources

--- src/testApp.h
+++ src/testApp.h
@@ -1,54 +1,38 @@
+/*
+
+    Musical Tabletop Coding Framework
+
+    Copyright (c) 2010 Carles F. Julià <carles.fernandez@upf.edu>
+    Copyright (c) 2010 Daniel Gallardo Grassot <daniel.gallardo@upf.edu>
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 #ifndef _TEST_APP
 #define _TEST_APP
 
 #include "TableApp.hpp"
 #include "Graphic.hpp"
-
-///Example of a class that receives events from direct objects and direct fingers
-///inheritance: dummylistener-->OnTable-->CanDirectObjects-->CanDirectFingers-->Graphic
-//#include "InputGestureDirectObjects.hpp"
-//#include "InputGestureDirectFingers.hpp"
-//class dummylistener: public OnTable< tuio::CanDirectObjects < tuio::CanDirectFingers < Graphic > > >
-//{
-//    public:
-//    virtual void newCursor(tuio::DirectFinger *){std::cout << "added direct cur" << std::endl;}
-//    virtual void removeCursor(tuio::DirectFinger *){std::cout << "removed direct cur" << std::endl;}
-//    virtual void newObject(tuio::DirectObject *){std::cout << "added direct obj" << std::endl;}
-//    virtual void removeObject(tuio::DirectObject *){std::cout << "removed direct obj" << std::endl;}
-//};
-
-
-///Example of a class that draws a circle at the middle of the screen
-/// it rewrites all methods from 'graphic': draw, update and resize
-//#include "GlobalConfig.hpp"
-//class dummyGraphic: public Graphic
-//{
-//    public:
-//        virtual void draw(){
-//            ofCircle(GlobalConfig::getWidth() /2,GlobalConfig::getHeight()/2,0.04);
-//        }
-//        virtual void update(){}
-//        virtual void resize(int w, int h){}
-//};
-
-///Example of a class that draws a circle where the last finger added is
-//#include "InputGestureDirectFingers.hpp"
-//class dummyListenerGraphic: public OnTable < tuio::CanDirectFingers < Graphic > >
-//{
-//    tuio::DirectFinger * finger;
-//    public:
-//        dummyListenerGraphic():finger(NULL){}
-//        virtual void draw(){
-//            if(finger != NULL) ofCircle(finger->getX(),finger->getY(),0.02f);
-//        }
-//        virtual void resize(int w, int h){}
-//        virtual void newCursor(tuio::DirectFinger * _finger){
-//            finger = _finger;
-//        }
-//        virtual void removeCursor(tuio::DirectFinger * f){
-//            if(f == finger) finger = NULL;
-//        }
-//};
 
 class testApp : public TableApp{
 	public:
