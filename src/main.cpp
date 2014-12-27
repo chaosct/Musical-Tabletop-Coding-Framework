d210abca59b4618e27d1b385dc7fcb7ec49af68e
Zeken <eszeken@hotmail.com>
2014-12-27 15:50:19 +0100

    Don't force to use GLUT, let OpenFrameworks decide

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "ofMain.h"
 #include "testApp.h"
-#include "ofAppGlutWindow.h"
 #include "ofxGlobalConfig.hpp"
 
 
@@ -10,12 +9,13 @@ int main( )
     //uncomment this for the final bundled version, it will contain de data folder into the osx bundle
     //ofSetDataPathRoot("../Resources/data/");
     //END OSX ONLY
-    ofAppGlutWindow window;
-    int width = ofxGlobalConfig::getRef("PROGRAM:WIDTH",1024);
-    int height = ofxGlobalConfig::getRef("PROGRAM:HEIGHT",768);
-    if(ofxGlobalConfig::getRef("PROGRAM:FULLSCREEN",0))
-        ofSetupOpenGL(&window, width ,height, OF_GAME_MODE);
-    else
-        ofSetupOpenGL(&window, width ,height, OF_WINDOW);
-	ofRunApp( new testApp());
+
+    int width = ofxGlobalConfig::getRef("PROGRAM:WIDTH", 1024);
+    int height = ofxGlobalConfig::getRef("PROGRAM:HEIGHT", 768);
+    if(ofxGlobalConfig::getRef("PROGRAM:FULLSCREEN", 0)){
+        ofSetupOpenGL(width, height, OF_GAME_MODE);
+    }else{
+        ofSetupOpenGL(width, height, OF_WINDOW);
+    }
+    ofRunApp(new testApp());
 }
